a23dd385307dcdca0e7e9e12dcc25a93debab514
Shashank <shashankverma9919310939@gmail.com>
2024-02-16 13:54:14 +0530

    js

---
+++ keywords.js
@@ -0,0 +1,11 @@
+
+a = 22;
+if (a % 2 === 0) {
+    console.log("even");
+    num= 88;
+} 
+else {
+    console.log("odd");
+}
+
+console.log(num);
---
+++ script.js
@@ -0,0 +1,28 @@
+
+// myname="Shashank";
+// empty=true;
+// console.log(myname);
+// console.log(empty);
+
+// console.log("Welcome to JavaScript");
+
+// console.log("Kya haal hai bhai mere")
+
+// a=10;
+// b=25;
+// console.log(a+b);
+
+// empty=false
+// myname= "shashank"
+// console.log(typeof a);
+// console.log(typeof empty);
+// console.log(typeof myname);
+
+
+// console.log(empty + myname);
+// console.log(a == myname);
+// console.log(1 == "1");
+
+
+
+
