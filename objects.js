fe1856d568dfcac11a666a78d344ba2b163ed7d3
Shashank <shashankverma9919310939@gmail.com>
2024-02-24 13:19:28 +0530

    object-js

--- objects.js
+++ objects.js
@@ -1,4 +1,54 @@
 
-const user  = {name:'Raju', age:25, email:"rajuji99@gmail.com", pass:"rajupass"};
+// const user  = {name:'Raju', age:25, email:"rajuji99@gmail.com", pass:"rajupass"};
 
-const
+// console.log(user);
+
+// console.log(user.name);
+
+// console.log(user['pass']);
+// user.pass = 'raju-changed-pass'
+
+// console.log(user['pass']);
+
+// user.address= "raju-add";
+// console.log(user.address);
+
+// console.log(Object.keys(user));
+
+// const smartphone = {
+//     brand:'Iqoo',
+//     model:'neo 7 pro',
+//     price:32999,
+//     color:"mat-red-orange"
+// };
+
+// console.log(smartphone);
+
+// console.log(smartphone.color.length);
+
+const phonelist = [
+    {
+        brand: 'Iqoo',
+        model: 'neo 7 pro',
+        price: 32999,
+        color: "mat-red-orange"
+    }, 
+    {
+        brand: 'Redmi',
+        model: '7 pro',
+        price: 20999,
+        color: "red-orange"
+    }, 
+    {
+        brand: 'Redmi',
+        model: '7',
+        price: 15999,
+        color: "mat-orange"
+    }
+];
+
+console.log(phonelist.length);
+
+console.log(phonelist[2].price);
+
+console.log(phonelist[2].color.length);
